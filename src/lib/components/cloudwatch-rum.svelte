3ad1c3f97d45c5be4afe380e38d527470cf83128
Christopher Eby <kreed@kreed.org>
2025-09-07 17:46:20 -0700

    refactor: direct request to rum

--- src/lib/components/cloudwatch-rum.svelte
+++ src/lib/components/cloudwatch-rum.svelte
@@ -1,21 +1,17 @@
 <script lang="ts">
 import { AwsRum } from "aws-rum-web";
 import { onMount } from "svelte";
-import { page } from "$app/state";
 import { PUBLIC_RUM_APP_ID } from "$env/static/public";
-
-// RUM endpoint - proxied through CloudFront
-const RUM_ENDPOINT = `${page.url.origin}/rum`;
 
 onMount(() => {
   try {
     const config = {
       sessionSampleRate: 1.0,
-      endpoint: RUM_ENDPOINT,
       telemetries: ["errors", "performance", "http"],
-      allowCookies: false, // GDPR compliance
+      allowCookies: true,
       disableAutoPageView: false,
       enableXRay: false,
+      signing: false,
       logLevel: "ERROR",
     };
 
